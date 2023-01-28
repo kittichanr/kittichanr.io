f8ed2ae041726210b8fd1f976e3c1ad64c4b2624
petchkubb <petchkubbb@gmail.com>
2023-01-29 06:43:53 +0700

    feat: add Nav and Footer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+	reactStrictMode: true,
+	swcMinify: true,
+	experimental: {
+		fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }],
+	},
 }
 
 module.exports = nextConfig
