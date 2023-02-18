d6161805bc146aaf89652969c0ba36e72aa2852c
petchkubb <petchkubbb@gmail.com>
2023-02-18 14:38:04 +0700

    fix: redesign home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { ArrowIcon } from 'components/icons'
+import { about, avatar, bio, name, quote } from 'lib/info'
 import Image from 'next/image'
 
 export const metadata = {
@@ -21,35 +23,45 @@ export const metadata = {
 
 export default function HomePage() {
 	return (
-		<div className="flex flex-col justify-center items-start max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pb-16">
-			<div className="flex flex-col-reverse sm:flex-row items-start">
-				<div className="flex flex-col pr-8">
-					<h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white">
-						Petch Kittichan
-					</h1>
-					<h2 className="text-gray-700 dark:text-gray-200 mb-4">
-						Software Engineer Experience at <span className="font-semibold">LMWN</span>
-					</h2>
-					<p className="italic text-gray-600 dark:text-gray-400 mb-4">
-						“Any fool can write code that a computer can understand. Good programmers write code
-						that humans can understand.“ - Martin Fowler
-					</p>
-					<p className="italic text-gray-600 dark:text-gray-400 mb-16">
-						“First, solve the problem. Then, write the code.” - John Johnson
-					</p>
-				</div>
-				<div className="w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 mr-auto">
-					<Image
-						alt="Petch Kittichan"
-						height={176}
-						width={176}
-						src="/avatar.jpg"
-						sizes="30vw"
-						priority
-						className="rounded-xl filter"
-					/>
+		<section className="">
+			<h1 className="font-bold text-3xl font-serif">{name}</h1>
+			<h2 className="text-gray-700 dark:text-gray-200 my-4">{about()}</h2>
+			<div className="flex items-start md:items-center my-8 flex-col md:flex-row">
+				<Image
+					alt={name}
+					className="rounded-xl"
+					src={avatar}
+					placeholder="blur"
+					width={150}
+					priority
+				/>
+				<div className="mt-8 md:mt-0 ml-0 md:ml-6 space-y-2 text-neutral-500 dark:text-neutral-400">
+					{quote()}
 				</div>
 			</div>
-		</div>
+			<h2 className="text-gray-700 dark:text-gray-200 mb-4">{bio()}</h2>
+			<ul className="flex flex-col md:flex-row mt-8 space-x-0 md:space-x-4 space-y-2 md:space-y-0 font-sm text-neutral-500 dark:text-neutral-400">
+				<li>
+					<a
+						className="flex items-center hover:text-neutral-700 dark:hover:text-neutral-200 transition-all"
+						rel="noopener noreferrer"
+						target="_blank"
+						href="https://github.com/kittichanr">
+						<ArrowIcon />
+						<p className="h-7">follow me on github</p>
+					</a>
+				</li>
+				<li>
+					<a
+						className="flex items-center hover:text-neutral-700 dark:hover:text-neutral-200 transition-all"
+						rel="noopener noreferrer"
+						target="_blank"
+						href="">
+						<ArrowIcon />
+						<p className="h-7">contract me in email</p>
+					</a>
+				</li>
+			</ul>
+		</section>
 	)
 }
---
+++ lib/info.tsx
@@ -0,0 +1,35 @@
+import me from '../app/avatar.jpg'
+
+export const name = 'Petch Kittichan'
+export const avatar = me
+export const about = () => {
+	return (
+		<>
+			{"👋, I'm Petch. Software Engineer Experience at"} <b>LMWN</b>{' '}
+			{"I'm work in restaurant solution team contribute on wongnai merchant app (WMA)"}
+		</>
+	)
+}
+export const bio = () => {
+	return (
+		<>
+			{"I'm passionate about frontend development.\n"}
+			{'I like to learn about a new thing in javascript especially React framework.\n'}
+			{"I'm currently learning React, Apollo Client, Golang."}
+		</>
+	)
+}
+export const quote = () => {
+	return (
+		<>
+			<p className="italic text-gray-600 dark:text-gray-400 mb-4">
+				{'“Any fool can write code that a computer can understand. '}
+				{'Good programmers write code that humans can understand.“ - '}
+				<b>Martin Fowler</b>
+			</p>
+			<p className="italic text-gray-600 dark:text-gray-400 mb-16">
+				“First, solve the problem. Then, write the code.” - <b>John Johnson</b>
+			</p>
+		</>
+	)
+}
